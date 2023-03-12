909c3801de7e3e4dd0eb77e7bf65d35c9ec524fe
Alexandr Tishkin <zuluwar6@gmail.com>
2023-03-12 17:11:22 +0300

    Fixed time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 def get_tagged_questions(tag, fromdate):
     response = requests.get(url='https://api.stackexchange.com/2.3/questions', params={'site': 'stackoverflow', 'tagged': tag, 'fromdate': fromdate}).json()
@@ -6,4 +7,5 @@ def get_tagged_questions(tag, fromdate):
     print(f'Найдены следующие вопросы:\n{questions}')
 
 if __name__ == '__main__':
-    get_tagged_questions('python', '1678406400')
+    date_two_days_back = int(time.time()) - 2 * 24 * 60 * 60
+    get_tagged_questions('python', date_two_days_back)
